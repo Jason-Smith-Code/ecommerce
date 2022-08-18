94b99d0bb4721097c12148eb500c5d63f11d4564
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-08-18 22:33:37 +0100

    added basic components to the app

---
+++ front-end/src/components/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "../header/Header";
+import { Footer } from "../footer/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
