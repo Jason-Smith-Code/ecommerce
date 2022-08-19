93b71abbb78cd68dc5ce19676492eaf46da183e0
jason-smith-code <jasonsmithcode@gmail.com>
2022-08-19 14:56:49 +0100

    Update Footer.jsx
    added some place holder details for footer section

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -1,28 +1,35 @@
 import './Footer.css';
+import { Link } from "react-router-dom";
+
+
 export const Footer = () => {
     return (
         <footer>
             <div className='footer-container'>
                 <div className='footer-block'>
                     <h4 className="footer-heading">Business Name</h4>
-                    <a href=".">Address</a>
-                    <a href=".">Email</a>
-                    <a href=".">Phone</a>
-                    <a href=".">About</a>
+                    <p>business address</p>
+                    <p>business email address</p>
+                    <p>business phone</p>
                 </div>
                 <div className='footer-block'>
                     <h4 className="footer-heading">My Account</h4>
-                    {/* If logged in */}
-                    <a href=".">Details</a>
-                    <a href=".">My Orders</a>
                     {/* if logged out */}
-                    <a href=".">Sign in</a>
+                    <Link to="/sign-in">Sign in / Register</Link>
+                    {/* If logged in */}
+                    <Link to="/account">My Details</Link>
+                    <Link to="/orders">Orders</Link>
                 </div>
                 <div className='footer-block'>
                     <h4 className="footer-heading">Service</h4>
-                    <a href=".">Delivery</a>
-                    <a href=".">Payments</a>
-                    <a href=".">Terms and Conditions</a>
+                    <Link to="/delivery">Delivery</Link>
+                    <Link to="/payments">Payments</Link>
+                    <Link to="/terms-conditions">Terms and Conditions</Link>
+                </div>
+                <div className='footer-block'>
+                    <h4 className="footer-heading">Navigation</h4>
+                    <Link to="/cart">Cart</Link>
+                    <Link to="/checkout">Checkout</Link>
                 </div>
             </div>
         </footer>
