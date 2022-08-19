2a15a374d4719271ac37bc0c3760d85476e04622
jason-smith-code <jasonsmithcode@gmail.com>
2022-08-19 14:56:35 +0100

    Update App.js
    imports lots of new pages to use with routes

--- front-end/src/components/app/App.js
+++ front-end/src/components/app/App.js
@@ -1,12 +1,40 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
+import { HomePage } from '../../routes/homePage';
+import { CartPage } from "../../routes/CartPage";
+import { CategoryPage } from "../../routes/CategoryPage";
+import { ProductPage } from "../../routes/ProductPage";
+import { CheckOutPage } from "../../routes/CheckOutPage";
+import { SignInPage } from "../../routes/SignInPage";
+import { UserAccountPage } from "../../routes/UserAccountPage";
+import { DeliveryPage } from "../../routes/DeliveryPage";
+import { OrdersPage } from "../../routes/OrdersPage";
+import { PaymentsPage } from "../../routes/PaymentsPage";
+import { TermsConditionsPage } from "../../routes/TermsConditionsPage";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="category" element={<CategoryPage />} />
+            <Route path="product" element={<ProductPage />} />
+            <Route path="checkout" element={<CheckOutPage />} />
+            <Route path="sign-in" element={<SignInPage />} />
+            <Route path="account" element={<UserAccountPage />} />
+            <Route path="delivery" element={<DeliveryPage />} />
+            <Route path="orders" element={<OrdersPage />} />
+            <Route path="payments" element={<PaymentsPage />} />
+            <Route path="terms-conditions" element={<TermsConditionsPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
