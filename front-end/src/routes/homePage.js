9ba25982beeadd39876cae99eeb8547ee08fd0b2
jason-smith-code <jasonsmithcode@gmail.com>
2022-08-19 15:28:44 +0100

    Update homePage.js

--- front-end/src/routes/homePage.js
+++ front-end/src/routes/homePage.js
@@ -6,24 +6,32 @@ export const HomePage = () => {
         // if product is marked as featured, display them
         return products.map(product => {
             return (
-                <div key={product._id}>
-                    <img alt="product" src={product.image}></img>
+                <div className="product-container" key={product._id}>
+                    {/* Wrap image in link */}
+                    <img alt="product" width="200px"src={product.image}></img>
                     <p>{product.title}</p>
                     <p>{product.price}</p>
                     <p>{product.description}</p>
+                    {/* add link to product page */}
+                    <button>View product</button>
+                    <button>Add to cart</button>
                 </div>
             )
         })
     }
 
-
     return (
         <div className="content-container">
-            <h1>Store Name</h1>
-            <p>Some text about the business</p>
-
-            <h2>Featured products</h2>
-            {displayFeaturedProducts()}
+            <section>
+                <h1>Store Name</h1>
+                <p>Some text about the business</p>
+            </section>
+            <section>
+                <h2>Featured products</h2>
+                <div className="products-grid">
+                    {displayFeaturedProducts()}
+                </div>
+            </section>
         </div>
     )
 }
