80342073f9134e7b7f22f8062c4ddbcaccec38d4
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-08-19 16:56:47 +0100

    update

--- front-end/src/routes/CartPage.js
+++ front-end/src/routes/CartPage.js
@@ -1,7 +1,8 @@
 export const CartPage = () => {
-    return (
-        <div className="content-container">
-            <p>CartPage</p>
-        </div>
-    )
-}
+  return (
+    <div className="content-container">
+      <p>CartPage</p>
+      <p>CartPage</p>
+    </div>
+  );
+};
