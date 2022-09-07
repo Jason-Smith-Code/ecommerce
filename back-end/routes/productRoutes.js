b8294e98f18d4c8ede182c16fdb6ad863426b423
jason-smith-code <jasonsmithcode@gmail.com>
2022-09-07 15:17:14 +0100

    Create productRoutes.js
    created a route for products

---
+++ back-end/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Product from '../models/productModel.js';
+import asyncHandler from 'express-async-handler';
+
+const router = express.Router();
+
+// @description :   Fetch all products
+// @route :         GET /api/products
+// @access :        Public
+
+router.get("/", asyncHandler(async(request, response) => {
+    const products = await Product.find({})
+    // convert the products to json
+    response.json(products);
+}));
+  
+// @description :   Fetch single products
+// @route :         GET /api/products/:id
+// @access :        Public
+
+router.get("/:id", asyncHandler(async(request, response) => {
+// get the product by its id
+const product = await Product.findById(request.params.id)
+
+    // check to make sure there is a product
+    if(product) {
+        // conver the products to json
+        response.json(product);
+    } else {
+        response.status(404).json({ message: 'product not found'})
+    }
+}));
+
+export default router;
