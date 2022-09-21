2f1156874bf461f2116cd10d88676a7bc1a083dd
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-09-21 08:44:13 +0100

    Update server.js
    added some comments, added middleware to change default error message type from html to JSON. Also converted the error status code from 200 to 500 as sometimes a 200 status code can still contain errors

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 import connectDB from "./config/db.js";
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 connectDB();
 
 // initialize express
 const app = express();
-// get port from env file
+
+// get port from env file or set it to a default port
 const PORT = process.env.PORT || 5000;
 
-// listen on port
+// listen on port, confirm listen with a console log
 app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 );
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
 // if we get a response then
 app.get("/", (request, response) => {
@@ -23,3 +27,16 @@ app.get("/", (request, response) => {
   response.send("API is running...");
 });
 
+// Error middleware - must be written last.
+// Check: http://expressjs.com/en/guide/error-handling.html
+
+app.use((err, request, res, next) => {
+  // May get 200 responses even though there is an error, convert 200 to a 500 response
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    // hide stack information if site is in production mode (NODE_ENV)
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
