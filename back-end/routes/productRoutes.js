7ef6881ff5579b24acf329b1b0816a93b39bd46a
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-09-21 08:43:07 +0100

    converted error message from html to JSON

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler';
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
@@ -8,27 +8,33 @@ const router = express.Router();
 // @route :         GET /api/products
 // @access :        Public
 
-router.get("/", asyncHandler(async(request, response) => {
-    const products = await Product.find({})
+router.get(
+  "/",
+  asyncHandler(async (request, response) => {
+    const products = await Product.find({});
     // convert the products to json
     response.json(products);
-}));
-  
+  })
+);
+
 // @description :   Fetch single products
 // @route :         GET /api/products/:id
 // @access :        Public
 
-router.get("/:id", asyncHandler(async(request, response) => {
-// get the product by its id
-const product = await Product.findById(request.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (request, response) => {
+    // get the product by its id
+    const product = await Product.findById(request.params.id);
 
     // check to make sure there is a product
-    if(product) {
-        // conver the products to json
-        response.json(product);
+    if (product) {
+      // conver the products to json
+      response.json(product);
     } else {
-        response.status(404).json({ message: 'product not found'})
+      response.status(404).json({ message: "product agasgadg dnot found" });
     }
-}));
+  })
+);
 
 export default router;
