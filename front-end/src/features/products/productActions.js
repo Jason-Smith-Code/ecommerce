3e606c68b63882c621aa5d9011a321085547c9ce
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-09-22 16:58:00 +0100

    Update productActions.js
    fixed errors in console

--- front-end/src/features/products/productActions.js
+++ front-end/src/features/products/productActions.js
@@ -1,25 +1,27 @@
-import { productListRequest, productListSuccess, productListFail } from "./productsSlice";
+import {
+  productListRequest,
+  productListSuccess,
+  productListFail,
+} from "./productsSlice";
 import axios from "axios";
 
 // fetch products from database
 export const fetchProductList = () => async (dispatch) => {
-    try {
-        dispatch({
-            type: productListRequest
-        });
-
-        const { data } = await axios.get('/api/products');
-
-        dispatch({
-            type: productListSuccess,
-            payload: data
-        });
-
-    } catch (error) {
-        
-        dispatch({
-            type: productListFail,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        });
-    }
-}
+  try {
+    // dispatch the request
+    dispatch(productListRequest);
+    // store the data from the api
+    const { data } = await axios.get("/api/products");
+    // dispatch the data
+    dispatch(productListSuccess(data));
+  } catch (error) {
+    // dispatch the error, if there is an error response display that, otherwise display the error message
+    dispatch(
+      productListFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
