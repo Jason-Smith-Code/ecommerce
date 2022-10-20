fbd3622a55234a7b8445d630d5c703a0a48093be
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 11:00:33 +0100

    Update productsSlice.js
    corrected syntax

--- front-end/src/features/products/productsSlice.js
+++ front-end/src/features/products/productsSlice.js
@@ -1,37 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: [],
-  loading: false,
-  error: ""
-}
+  error: "",
+};
 
 export const productsSlice = createSlice({
-  name: 'productList',
+  name: "productList",
   initialState,
   reducers: {
-    productListRequest: (state, action) => {
-        state.loading = true;
-        state.products = [];
+    productListRequest(state, action) {
+      state.loading = true;
+      state.products = [];
     },
-    productListSuccess: (state, action) => {
-        state.loading = false;
-        state.products = action.payload;
+    productListSuccess(state, action) {
+      state.loading = false;
+      state.products = action.payload;
     },
-    productListFail: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
+    productListFail(state, action) {
+      state.loading = false;
+      state.error = action.payload;
     },
-    fetchProducts: (state,action) => {
-
-    }
   },
-})
+});
 
 // function to get the product list
 export const getProductList = (state) => state.productList.products;
 
 // Action creators are generated for each case reducer function
-export const { productListRequest, productListSuccess, productListFail } = productsSlice.actions
+export const { productListRequest, productListSuccess, productListFail } =
+  productsSlice.actions;
 
-export default productsSlice.reducer
+export default productsSlice.reducer;
