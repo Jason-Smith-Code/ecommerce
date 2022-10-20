6b7ec223354f007ba51d300503d822ccad5976fd
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 11:01:42 +0100

    Update homePage.js

--- front-end/src/routes/homePage.js
+++ front-end/src/routes/homePage.js
@@ -1,29 +1,32 @@
-import React, { useEffect} from "react";
-import { fetchProductList} from "../features/products/productActions";
-import {getProductList} from '../features/products/productsSlice'
+import React, { useEffect } from "react";
+import { fetchProductList } from "../features/products/productActions";
+import { getProductList } from "../features/products/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { ProductList } from "../components/product/ProductList";
 
 export const HomePage = () => {
   const dispatch = useDispatch();
   const productList = useSelector(getProductList);
 
   useEffect(() => {
-    dispatch(fetchProductList())
-  }, [dispatch])
+    // since this page is displaying products, we need to fetch them
+    dispatch(fetchProductList());
+  }, [dispatch]);
 
-
-
+  // map through products, if product.featured === true then display the product
+  const mappedProductList = productList.map((product) => {
+    return <ProductList key={product._id} product={product} />;
+  });
 
   return (
     <div className="content-container">
       <section>
         <h1>Store Name</h1>
-        <p>products amount = {productList.length}</p>
         <p>Some text about the business</p>
       </section>
       <section>
         <h2>Featured products</h2>
-        <div className="products-grid"></div>
+        <div className="products-grid">{mappedProductList}</div>
       </section>
     </div>
   );
