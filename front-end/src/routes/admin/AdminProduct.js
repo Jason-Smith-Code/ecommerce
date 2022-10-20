60656338158e3ad64bd72ad9c60e95624e265d92
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 11:01:26 +0100

    Create AdminProduct.js

---
+++ front-end/src/routes/admin/AdminProduct.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ProductForm } from "../../components/forms/ProductForm";
+
+export const AdminProduct = () => {
+  return (
+    <div>
+      <ProductForm />
+    </div>
+  );
+};
