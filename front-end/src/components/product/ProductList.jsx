b32f36109327d01262f7c5047b68090c009844c1
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 10:59:56 +0100

    Update ProductList.jsx
    created a product list component to use in several parts of the site

--- front-end/src/components/product/ProductList.jsx
+++ front-end/src/components/product/ProductList.jsx
@@ -1,6 +1,20 @@
-export const ProductList = () => {
+import './ProductList.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from '../button/Button';
+
+export const ProductList = ({product}) => {
     return (
-        <></>
+        <div className="product-grid-item">
+            <img alt="product" className='product-list-image' src={product.image}></img>
+            <h2>{product.title}</h2>
+            <p>£{product.price}</p>
+            {product.stock === 0 ? <p>out of stock</p> : <p>in stock</p> }
+            <div className='product-list-button-row'>
+                <Button buttonClass="cart" text="Add"></Button>
+                <Link to={`/product/${product._id}`}><Button buttonClass="view" text="View"></Button></Link>
+            </div>
+        </div>
     )
                                                                                                    
 }
