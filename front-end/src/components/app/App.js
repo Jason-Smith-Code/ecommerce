32b48e8495b51e13ac3baf4d2254a33311d3b543
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 10:57:30 +0100

    Update App.js
    split the routes into a public and admin set of routes

--- front-end/src/components/app/App.js
+++ front-end/src/components/app/App.js
@@ -1,39 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Header } from "../header/Header";
-import { Footer } from "../footer/Footer";
-import { HomePage } from "../../routes/homePage";
-import { CartPage } from "../../routes/CartPage";
-import { CategoryPage } from "../../routes/CategoryPage";
-import { ProductPage } from "../../routes/ProductPage";
-import { CheckOutPage } from "../../routes/CheckOutPage";
-import { SignInPage } from "../../routes/SignInPage";
-import { UserAccountPage } from "../../routes/UserAccountPage";
-import { DeliveryPage } from "../../routes/DeliveryPage";
-import { OrdersPage } from "../../routes/OrdersPage";
-import { PaymentsPage } from "../../routes/PaymentsPage";
-import { TermsConditionsPage } from "../../routes/TermsConditionsPage";
+import { Public } from "../../routes/Public";
+import { Admin } from "../../routes/admin/Admin";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/" element={<HomePage />} exact />
-        <Route path="cart" element={<CartPage />} />
-        <Route path="category" element={<CategoryPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="checkout" element={<CheckOutPage />} />
-        <Route path="sign-in" element={<SignInPage />} />
-        <Route path="account" element={<UserAccountPage />} />
-        <Route path="delivery" element={<DeliveryPage />} />
-        <Route path="orders" element={<OrdersPage />} />
-        <Route path="payments" element={<PaymentsPage />} />
-        <Route path="terms-conditions" element={<TermsConditionsPage />} />
+        <Route path="/*" element={<Public />} />
+        <Route path="/admin/*" element={<Admin />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
