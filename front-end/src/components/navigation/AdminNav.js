af2715a364762fa703c85094e262e7c8339b55ea
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-20 10:58:53 +0100

    Create AdminNav.js
    created an admin side menu

---
+++ front-end/src/components/navigation/AdminNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AdminNav.css";
+
+export const AdminNav = () => {
+  return (
+    <nav id="admin-menu">
+      <ul>
+        <li>Overview</li>
+        <li>Products</li>
+        <li>Categories</li>
+        <li>Orders</li>
+        <li>Settings</li>
+      </ul>
+    </nav>
+  );
+};
