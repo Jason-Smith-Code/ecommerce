0b9946c5db3c6d20bb38db447a4e122510a746c2
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-10-24 19:49:55 +0100

    Update AdminNav.js
    improved admin menu

--- front-end/src/components/navigation/AdminNav.js
+++ front-end/src/components/navigation/AdminNav.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./AdminNav.css";
+import { Link } from "react-router-dom";
 
 export const AdminNav = () => {
   return (
     <nav id="admin-menu">
       <ul>
-        <li>Overview</li>
-        <li>Products</li>
-        <li>Categories</li>
-        <li>Orders</li>
-        <li>Settings</li>
+        <li>
+          <Link className="admin-link" to="/admin/">
+            overview
+          </Link>
+        </li>
+        <li>
+          <Link className="admin-link" to="/admin/create-product">
+            Create Product
+          </Link>
+        </li>
+        <li>
+          <Link className="admin-link" to="/admin/">
+            Categories
+          </Link>
+        </li>
+        <li>
+          <Link className="admin-link" to="/admin/">
+            Orders
+          </Link>
+        </li>
+        <li>
+          <Link className="admin-link" to="/admin/">
+            Settings
+          </Link>
+        </li>
       </ul>
     </nav>
   );
