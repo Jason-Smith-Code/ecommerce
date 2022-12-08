39d09528087cb62a90200c32b40dff67bc64d860
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-12-08 11:30:00 +0000

    Update store.js

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import productListReducer from './features/products/productsSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import productListReducer from "./features/products/productsSlice";
+import productDetailReducer from "./features/products/productDetailSlice";
+import cartListReducer from "./features/products/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    productList: productListReducer
+    productList: productListReducer,
+    productDetail: productDetailReducer,
+    cartList: cartListReducer,
   },
-})
+});
