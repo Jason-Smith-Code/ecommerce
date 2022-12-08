f05f02f35f1b0d60fc3187d7ab5a25d9fbabd43b
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-12-08 11:29:22 +0000

    Update ProductForm.js

--- front-end/src/components/forms/ProductForm.js
+++ front-end/src/components/forms/ProductForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Forms.css";
+// import { sendProduct } from "../../features/products/productActions";
 
 export const ProductForm = () => {
   // Manage local state, these are the attributes we need to send.
@@ -17,38 +18,41 @@ export const ProductForm = () => {
   });
 
   // These methods will update the state properties.
-  function updateForm(value) {
-    return setProductAttributes((prev) => {
-      return { ...prev, ...value };
-    });
-  }
+  // function updateForm(value) {
+  //   return setProductAttributes((prev) => {
+  //     return { ...prev, ...value };
+  //   });
+  // }
 
   async function handleSubmit(e) {
     e.preventDefault();
     const newProduct = { ...productAttributes };
     console.log(newProduct);
-
-    // reset form
-    setProductAttributes({
-      title: "",
-      description: "",
-      images: [],
-      coverImage: "",
-      brand: "",
-      category: "",
-      price: 0.0,
-      rrp: 0.0,
-      quantity: 0,
-    });
+    let jsonProject = JSON.stringify(newProduct);
+    console.log(jsonProject);
+    // sendProduct(jsonProject);
   }
 
   return (
     <form
+      method="post"
       onSubmit={(e) => {
         handleSubmit(e);
       }}
     >
-      <div className="form-row">
+      <div>
+        <p>title: {productAttributes.title}</p>
+        <p>description: {productAttributes.description}</p>
+        <p>images: {productAttributes.images}</p>
+        <p>coverImage: {productAttributes.coverImage}</p>
+        <p>Brand: {productAttributes.brand}</p>
+        <p>category: {productAttributes.category}</p>
+        <p>price: {productAttributes.price}</p>
+        <p>rrp: {productAttributes.rrp}</p>
+        <p>quantity: {productAttributes.quantity}</p>
+        <p>user: {productAttributes.user}</p>
+      </div>
+      {/* <div className="form-row">
         <div className="form-col-1">
           <label>Title</label>
         </div>
@@ -75,106 +79,8 @@ export const ProductForm = () => {
             value={productAttributes.description}
             onChange={(e) => updateForm({ description: e.target.value })}
           />
-        </div>
-      </div>
-
-      {/* <div className="form-row">
-        <div className="form-col-1">
-          <label>Category</label>
-        </div>
-        <div className="form-col-2">
-          <select
-            name="category"
-            type="text"
-            placeholder="category"
-            value={productAttributes.category}
-            onChange={(e) => setProductAttributes({ category: e.target.value })}
-          >
-            <option value="cat1">Category 1</option>
-            <option value="cat2">Category 2</option>
-            <option value="cat3">Category 3</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-col-1">
-          <label>Brand</label>
-        </div>
-        <div className="form-col-2">
-          <select
-            name="category"
-            type="text"
-            placeholder="category"
-            value={productAttributes.brand}
-            onChange={(e) => setProductAttributes({ brand: e.target.value })}
-          >
-            <option value="cat1">Brand 1</option>
-            <option value="cat2">Brand 2</option>
-            <option value="cat3">Brand 3</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-col-1">
-          <label>price</label>
-        </div>
-        <div className="form-col-2">
-          <input
-            name="price"
-            type="number"
-            placeholder="0.00"
-            value={productAttributes.price}
-            onChange={(e) => setProductAttributes({ price: e.target.value })}
-          />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-col-1">
-          <label>rrp</label>
-        </div>
-        <div className="form-col-2">
-          <input
-            name="rrp"
-            type="number"
-            placeholder="0.00"
-            value={productAttributes.rrp}
-            onChange={(e) => setProductAttributes({ rrp: e.target.value })}
-          />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-col-1">
-          <label>stock</label>
-        </div>
-        <div className="form-col-2">
-          <input
-            name="stock"
-            type="number"
-            placeholder="0.00"
-            value={productAttributes.stock}
-            onChange={(e) => setProductAttributes({ stock: e.target.value })}
-          />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-col-1">
-          <label>active?</label>
-        </div>
-        <div className="form-col-2">
-          <input
-            name="true"
-            type="radio"
-            value={productAttributes.active}
-            onChange={(e) => setProductAttributes({ active: e.target.value })}
-          />
-        </div>
-      </div> */}
-
+        </div> */}
+      {/* </div> */}
       <input type="submit" value="Create Product" />
     </form>
   );
